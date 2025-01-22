869c0c159e4ac42bf91425ea6f0c36a352fc896a
Nayan Becker <nayan.becker@expressosaomiguel.com.br>
2025-01-22 08:36:37 -0300

    First Commit 🔥

---
+++ src/main/java/com/courses/api/crud_courses/CrudCoursesApplication.java
@@ -0,0 +1,13 @@
+package com.courses.api.crud_courses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudCoursesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/courses/api/crud_courses/CrudCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courses.api.crud_courses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
